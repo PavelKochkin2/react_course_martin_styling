b9d18c18ef90a7f5dec9eb491a577e3408641859
Pavel Kochkin <kach93@mail.ru>
2023-08-26 10:32:52 +0200

    added synamic styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isInputValid, setIsInputValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsInputValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setIsInputValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isInputValid ? "" : "invalid"}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
+        <div hidden={isInputValid}>Invalid input baby</div>
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
